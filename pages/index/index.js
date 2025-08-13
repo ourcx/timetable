aa01d05b051c2dc4006a5f705e36e7bdd82ccf1a
小海 <3277975910@qq.com>
2025-08-13 16:35:34 +0800

    feat: 首页和底部导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,47 +3,36 @@ const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia0
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
+    navList:[{
+      title:'课表',
+      icon:'/pubilc/容器.png',
+      path:'/pages/course/index'
     },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    {
+      title:'成绩',
+      icon:'/pubilc/容器 1.png',
+      path:'/pages/score/index'
+    },
+    {
+      title:'考勤',
+      icon:'/pubilc/容器 2.png',
+      path:'/pages/attendance/index'
+    },
+    {
+      title:'校历',
+      icon:'/pubilc/容器 3.png',
+      path:'/pages/calendar/index'
+    }
+  ]
   },
-  bindViewTap() {
+  onLoad(){
+
+  },
+  nav(e){
+    const index = e.currentTarget.dataset.index;
+    const path = this.data.navList[index].path;
     wx.navigateTo({
-      url: '../logs/logs'
+      url: path
     })
-  },
-  onChooseAvatar(e) {
-    const { avatarUrl } = e.detail
-    const { nickName } = this.data.userInfo
-    this.setData({
-      "userInfo.avatarUrl": avatarUrl,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  onInputChange(e) {
-    const nickName = e.detail.value
-    const { avatarUrl } = this.data.userInfo
-    this.setData({
-      "userInfo.nickName": nickName,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
+  }
 })
